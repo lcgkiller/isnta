756ebe16a1d8fc637a07edbcea370d001b5b8967
lcgkiller <lcgkiller@gmail.com>
2017-06-12 12:10:30 +0900

    first commit

---
+++ django_app/config/settings.py
@@ -0,0 +1,141 @@
+"""
+모듈 모음
+    회원관리 모듈(member/)
+    
+        팔로우
+        로그인
+        회원가입
+        친구찾기
+        친구추천
+        마이페이지
+            내가 올린글
+            프로필 관리    
+       
+        
+    글 관련 모듈(post/)
+       
+        뉴스피드
+        사진업로드
+        댓글달기
+        좋아요
+        해쉬태그
+
+
+    알림 관련 모듈(noti/)
+       
+        팔로워의 글 등록 알림
+        댓글알림
+    
+"""
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '!xvnwdrjvhr9-i+f8$ae^v1878()i)sy+)qujj0q%vo(eivb_e'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'django_extensions',
+
+    'post',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.11/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_URL = '/static/'
---
+++ django_app/post/models.py
@@ -0,0 +1,54 @@
+"""
+member application 생성
+    User 모델 구현
+        username, nickname
+이후 해당 User 모델을 Post나 Comment에서 author, User항목으로 참조
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    photo = models.ImageField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        User, related_name='like_posts'
+    )
+
+    tags = models.ManyToManyField('Tag')
+
+    def add_comment(self, user, content):
+        # 자신을 post로 갖고, 전달받은 user를 author로 가지며, content를 content 필드내용으로 넣는 Comment 객체를 생성
+        return self.comment_set.create(author=user, content=content)
+
+    def add_tag(self, tag_name):
+        # tags에 tag매개변수로 전달된 값(str) name으로 갖는 Tag객체를 (이미 존재하면) 가져오고 없으면 생성하여 자신의 tags에 추가
+
+        tag, tag_created = Tag.objects.get_or_create(name=tag_name)
+
+        if not self.tags.filter(name=tag_name).exists():
+            self.tags.add(tag)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(User)
+    photo = models.ImageField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    content = models.TextField(null=True, blank=True)
+    # user_nickname = User.user_nickname  # User 모델
+
+
+class Tag(models.Model):
+    # tag = models.TextField()
+    tag = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Tag({})'.format(self.tag)
