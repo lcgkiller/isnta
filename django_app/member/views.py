6dd1061277ae99f3ab091a03652d696810a51317
lcgkiller <lcgkiller@gmail.com>
2017-06-15 12:24:54 +0900

    로그인 기능 구현

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login as django_login
 
 # Create your views here.
 def login(request):
@@ -14,15 +14,21 @@ def login(request):
     #   config/urls.py에 member/urls.py를 include
     #       member/urls.py에 app_name설정으로 namespace 설정
 
-    username = request.POST.get('username', False)
-    password = request.POST.get('password', False)
-    user = authenticate(request, username=username, password=password)
     if request.method == "POST":
+        username = request.POST.get('username', False)
+        password = request.POST.get('password', False)
+        user = authenticate(request, username=username, password=password)
+
         if user is not None:
-            login(request, user)
+            # user 객체가 있는 경우, Django 세션을 이용해 request와 user객체를 이용해 로그인 처리를 진행한다.
+            # 이후의 request/response에서는 사용자가 인증된 상태로 통신이 이루어진다.
+            django_login(request, user)
             return redirect('posts:post_list')
         else:
             return HttpResponse("Login invalid!")
 
     else:
+        # 02. 이미 로그인 된 상태일 경우에는 post_list로 redirect
+        if request.user.is_authenticated:
+            return redirect("posts:post_list")
         return render(request, 'member/login.html')
